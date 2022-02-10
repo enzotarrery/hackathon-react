8de4b7a2df62023ea6fff7cbb35a5277ea139a6b
Antoine Marionneau <antoine.marionneau@etudiant.univ-lr.fr>
2022-02-10 21:56:30 +0100

    profile component integ WIP

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { AuthContext } from '../App'
-import '../assets/scss/modules/_profile.scss'
 
 
 
@@ -27,6 +26,8 @@ const Profile = () => {
             .then((response) => setPlaces(response))
             .catch((error) => console.warn(`ERROR (${error.code}) : ${error.message}.`));
     }
+
+    //HOOKS
 
     const [state, actions] = useContext(AuthContext)
     const [places, setPlaces] = useState([])
@@ -76,40 +77,38 @@ const Profile = () => {
                 else {
                     console.log(res);
                 }
-            })
+            }
+            )
     }
 
 
     return (
-        <section className='profile'>
+        <section className={loggedUser.role === "student" ? "profile" : "profile profile--dark"}>
+            <h1 className='profile__title'>Mon profil</h1>
+            <main className="profile__main">
+                <div className='profile__header'>
+                    <img className='profile__profil-picture' src={loggedUser.picture ? loggedUser.picture : "/img/default_pp.jpg"} alt="" />
+                    <button className='button'>Changer de photo</button>
+                    <button className='button' disabled={!loggedUser.picture}>Supprimer la photo</button>
+                </div>
 
-            <div className='profile__header'>
-                <h1>Mon profil</h1>
-                <img style={{ borderRadius: "50%", height: "250px", width: "250px" }} src={loggedUser.picture ? loggedUser.picture : "/img/default_pp.jpg"} alt="" />
-                <button>Changer de photo</button>
-                <button disabled={!loggedUser.picture}>Supprimer la photo</button>
-            </div>
 
-
-            <form className='secondCol' onSubmit={handleSubmit}>
-                <label htmlFor="firstName">Prénom</label>
-                <input type="text" name='firstName' id='firstName' value={formState.firstName} onChange={handleFormChange} />
-                <label htmlFor="lastName">Nom</label>
-                <input type="text" name='lastName' id='lastName' value={formState.lastName} onChange={handleFormChange} />
-                <label htmlFor="email">Email</label>
-                <input type="mail" name='email' id='email' value={formState.email} onChange={handleFormChange} />
-                <label htmlFor="password">Mot de passe</label>
-                <input type="password" name='password' id='password' value={formState.password} onChange={handleFormChange} />
-                <select onChange={handlePlaceChange} name="place" id="place" multiple={true} value={formState.favoritePlacesId}>
-                    {places.map(place => {
-                        return (
-                            <option key={place.id} value={place.id}>{place.name}</option>
-                        )
-                    })}
-                </select>
-                <button type="submit">Valider</button>
-            </form>
-        </section >
+                <form className='profile__form' onSubmit={handleSubmit}>
+                    <input className='profile__input' type="text" name='firstName' id='firstName' value={formState.firstName} onChange={handleFormChange} />
+                    <input className='profile__input' type="text" name='lastName' id='lastName' value={formState.lastName} onChange={handleFormChange} />
+                    <input className='profile__input' type="mail" name='email' id='email' value={formState.email} onChange={handleFormChange} />
+                    <input className='profile__input' type="password" name='password' id='password' value={formState.password} onChange={handleFormChange} />
+                    <select className='profile__input profile__input--select' onChange={handlePlaceChange} name="place" id="place" multiple={true} value={formState.favoritePlacesId}>
+                        {places.map(place => {
+                            return (
+                                <option className='profile__options' key={place.id} value={place.id}>{place.name}</option>
+                            )
+                        })}
+                    </select>
+                    <button className='button profile__submit' type="submit">Valider</button>
+                </form>
+            </main>
+        </section>
     );
 };
 
