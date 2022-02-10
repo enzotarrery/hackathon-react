467e48b0cdacb523774e12503c12acfff35220d8
fgroisil <flavien.groisillier@etudiant.univ-lr.fr>
2022-02-10 18:45:56 +0100

    add css header and styles page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header>
-            <Link to="/help" className='button'>Aide</Link>
-            <Link to="/logout" className='button button--primary'>Inscription</Link>
-            <Link to="/login" className='button'>Connexion</Link>
-        </header>
-    );
-}
+  return (
+    <header className="header">
+      <Link to="/help" className="button">
+        Aide
+      </Link>
+      <Link to="/logout" className="button button--primary">
+        Inscription
+      </Link>
+      <Link to="/login" className="button">
+        Connexion
+      </Link>
+    </header>
+  );
+};
 
 export default Header;
