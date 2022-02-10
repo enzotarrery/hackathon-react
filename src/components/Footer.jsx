47f2c926697ae2349175b52a8ddc9242b89574be
Enzo Tarrery <enzo.tarrery@etudiant.univ-lr.fr>
2022-02-10 11:13:44 +0100

    Add: legals & privacy policy pages + Update: footer with links to these pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer>
-          
+          <Link to='/legals'>Mentions légales</Link>
+          <Link to='/privacy'>Politique de Confidentialité</Link>
         </footer>
     );
 }
---
+++ src/views/PrivacyPolicy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConfidentialityPolicy = () => {
+  return (
+    <div>ConfidentialityPolicy</div>
+  )
+}
+
+export default ConfidentialityPolicy
